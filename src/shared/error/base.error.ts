6831ac4e08dda8aa934706beebf5a8dff02a9a57
Krishal Shah <krishals.001@gmail.com>
2024-05-05 17:47:07 +0530

    [Feat] Added API error

--- src/shared/error/base.error.ts
+++ src/shared/error/base.error.ts
@@ -1,10 +1,18 @@
+import { HttpStatusCode } from '@/types';
+
 export class BaseError extends Error {
   public readonly log: string;
   public readonly methodName: string;
   public readonly httpCode: number;
   public readonly isOperational: boolean;
 
-  constructor(log: string, message: string | unknown = log, methodName?: string, httpCode: number = 500, isOperational = true) {
+  constructor(
+    log: string,
+    message: string | unknown = log,
+    methodName?: string,
+    httpCode: HttpStatusCode = HttpStatusCode.INTERNAL_SERVER,
+    isOperational = true
+  ) {
     super(<string>message);
     Object.setPrototypeOf(this, new.target.prototype);
 
